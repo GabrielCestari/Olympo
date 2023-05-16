efb54e2b23c87229e347d71166ee9edca3f657e6
gabriel <gabriel.sanchez@sptech.school>
2023-05-16 19:08:24 -0300

    api certo

---
+++ siteindividual/web-data-viz-main/site/src/database/script-tabelas.sql
@@ -0,0 +1,32 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql - banco local - ambiente de desenvolvimento
+*/
+
+create database olympo;
+use olympo;
+
+create table usuario (
+idUsuario int primary key auto_increment,
+nome varchar(45),
+sobrenome varchar(45),
+email varchar(50),
+senha varchar(45)
+);
+/*
+comandos para criar usuário em banco de dados azure, sqlserver,
+com permissão de insert + update + delete + select
+*/
+
+CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
+WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
+DEFAULT_SCHEMA = dbo;
+
+EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
+@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
+
+EXEC sys.sp_addrolemember @rolename = N'db_datareader',
+@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
