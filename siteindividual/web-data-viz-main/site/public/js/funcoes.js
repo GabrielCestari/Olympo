98e14195a9c691985d2310ae375bb28788bea4f8
Allan-Batista <Allan.lima@bandtec.com.br>
2023-05-17 15:42:27 -0300

    tela usuario

--- siteindividual/web-data-viz-main/site/public/js/funcoes.js
+++ siteindividual/web-data-viz-main/site/public/js/funcoes.js
@@ -4,8 +4,13 @@ function validarSessao() {
 
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
+    var sobrenome =  sessionStorage.SOBRENOME_USUARIO;
+    var senha = sessionStorage.SENHA_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
+    var b_sobrenome = document.getElementById("b_sobrenome");
+    var b_email = document.getElementById("b_email");
+    var b_senha = document.getElementById("b_senha");
 
     if (email != null && nome != null) {
         // window.alert(`Seja bem-vindo, ${nome}!`);
